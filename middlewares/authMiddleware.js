72d576a18598f95596ebef0f52b8784d4f8faeff
Paulo Neto <prfneto@gmail.com>
2022-08-14 23:51:36 -0300

    Teste no authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,25 +1,21 @@
 import { JSONCookies } from 'cookie-parser';
 import jwt from'jsonwebtoken';
 
-const jwtScret = process.env.ACCESS_TOKEN_SECRET;
-
 const requireAuth = (req, res, next) =>{
-    try{
-        const token = req.cookies;
-
-        if(token){
-            jwt.verify(token, jwtSecret, (err, decodedToken) =>{
-                if(err){
-                    console.log(err.message);
-                    res.status(401).json({error: err.message});
-                }else{
-                    console.log(decodedToken);
-                    next();
-                }
-            })
-    }
-}
-    catch(e){
+    const token = req.body.token;
+    const jwtSecret = process.env.ACCESS_TOKEN_SECRET;
+    
+    if(token){
+        jwt.verify(token, jwtSecret, (err, decodedToken) =>{
+            if(err){
+                console.log(err.message);
+                res.status(401).json({error: err.message});
+            }else{
+                console.log(decodedToken);
+                next();
+            }
+        })
+    }else{
         res.status(401).json({error: 'no token'});
     }
 }
